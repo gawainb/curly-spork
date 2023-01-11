1797ac548ece13174274e92f1bd82304ddc5b472
Gawain Bracy II <bracy.gawain2014@gmail.com>
2023-01-11 12:20:37 -0500

    update to navbar
    - Added .SVG images to config
    - Images
    - Imported original header from previous repo
    - Added tx titles
    
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -16,6 +16,12 @@ export const SOCIAL_GITHUB = 'creativeplatform/creative-tv'
 
 export const ETH_CHAINS = [mainnet, goerli, sepolia, polygon, optimism, arbitrum]
 
+// Title text for the various transaction notifications.
+export const TRANSACTION_TITLES = {
+  transactionStarted: 'Local Transaction Started',
+  transactionSucceed: 'Local Transaction Completed',
+}
+
 export const SERVER_SESSION_SETTINGS = {
   cookieName: SITE_NAME,
   password: process.env.SESSION_PASSWORD ?? 'UPDATE_TO_complex_password_at_least_32_characters_long',
