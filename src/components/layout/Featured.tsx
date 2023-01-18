2aabab7bc12beca18dc9abd5defb0920621189de
Gawain Bracy II <bracy.gawain2014@gmail.com>
2023-01-17 21:25:41 -0500

    Featured Videos
    - Hero bug fixes.
    - Add featured video section.
    
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

--- src/components/layout/Featured.tsx
+++ src/components/layout/Featured.tsx
@@ -19,52 +19,9 @@ import { SITE_NAME } from 'utils/config'
 export default function HeroSection() {
   return (
     <Container maxW={'7xl'}>
-      <Stack align={'center'} spacing={{ base: 8, md: 10 }} py={{ base: 20, md: 28 }} direction={{ base: 'column', md: 'row' }}>
-        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-          <Heading lineHeight={1.1} fontWeight={600} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
-            <Text
-              as={'span'}
-              position={'relative'}
-              _after={{
-                content: "''",
-                width: 'full',
-                height: '30%',
-                position: 'absolute',
-                bottom: 1,
-                left: 0,
-                bg: '#EE774D',
-                zIndex: -1,
-              }}>
-              Record Once,
-            </Text>
-            <br />
-            <Text as={'span'} color={'#EE774D'}>
-              Use Everywhere!
-            </Text>
-          </Heading>
-          <Text color={'gray.500'}>
-            {SITE_NAME} is a decentralized live streaming platform that puts you in control of your content and earnings. Get paid 100% of streaming
-            revenue, have access to your own social token, and monetize your content into NFTs.
-          </Text>
-          <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: 'column', sm: 'row' }}>
-            <Button rounded={'full'} size={'lg'} fontWeight={'normal'} px={6} colorScheme={'orange'} bg={'#F2C57C'} _hover={{ bg: '#D9B06F' }}>
-              Get started
-            </Button>
-            <Button
-              rounded={'full'}
-              size={'lg'}
-              fontWeight={'normal'}
-              px={6}
-              colorScheme={'pink'}
-              bg={'#FF4583'}
-              _hover={{ bg: '#D93B6F' }}
-              leftIcon={<PlayIcon h={4} w={4} color={'gray.100'} />}>
-              How It Works
-            </Button>
-          </Stack>
-        </Stack>
-        <Flex flex={1} justify={'center'} align={'center'} position={'relative'} w={'full'}>
-          <Blob w={'150%'} h={'150%'} position={'absolute'} top={'-20%'} left={0} zIndex={-1} color={useColorModeValue('#FF4583', '#D93B6F')} />
+      <Stack align={'center'} spacing={{ base: 0, md: -10 }} py={{ base: 20, md: 28 }} direction={{ base: 'column', md: 'row' }}>
+        <Flex flex={1} justify={'left'} align={'center'} position={'relative'} w={'full'}>
+          {/* <Blob w={'150%'} h={'150%'} position={'absolute'} top={'-20%'} left={0} zIndex={-1} color={useColorModeValue('#FF4583', '#D93B6F')} /> */}
           <Box position={'relative'} height={'300px'} rounded={'2xl'} boxShadow={'2xl'} width={'full'} overflow={'hidden'}>
             <IconButton
               aria-label={'Play Button'}
@@ -88,6 +45,33 @@ export default function HeroSection() {
             />
           </Box>
         </Flex>
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading lineHeight={1.1} fontWeight={600} fontSize={{ base: '4xl', sm: '3xl', lg: '5xl' }} zIndex={-1}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: '#EE774D',
+                zIndex: -1,
+              }}>
+              Record. Watch. Brand.
+            </Text>
+            <br />
+            <Text as={'span'} color={'#EE774D'}>
+              Stream. Create. Inspire.
+            </Text>
+            <br />
+            <Text as={'span'} color={'#EE774D'}>
+              Engage. Dream. Enjoy.
+            </Text>
+          </Heading>
+        </Stack>
       </Stack>
     </Container>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Heading, ListItem, UnorderedList } from '@chakra-ui/react'
 import { Head } from 'components/layout/Head'
 import { LinkComponent } from 'components/layout/LinkComponent'
 import HeroSection from 'components/layout/HeroSection'
+import Featured from 'components/layout/Featured'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
 
       <main>
         <HeroSection />
+        <Featured />
         <UnorderedList>
           <ListItem>
             <LinkComponent href="/examples/sign">Sign & verify messages</LinkComponent>
