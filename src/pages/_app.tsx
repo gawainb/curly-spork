1932c8dcce623104b4c798255ad7d525822425f0
Alexander Greene <greenealexander036@gmail.com>
2023-01-21 10:04:54 -0600

    feat: remove thirdweb and use wagmi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Layout } from 'components/layout'
-import { Web3Provider } from 'providers/Web3'
 import { ChakraProvider } from 'providers/Chakra'
 import { useIsMounted } from 'hooks/useIsMounted'
 import { Seo } from 'components/layout/Seo'
+
+import { WagmiConfig, createClient } from 'wagmi'
+import { ConnectKitProvider, getDefaultClient } from 'connectkit'
+
+const wagmiClient = createClient(
+  getDefaultClient({
+    appName: 'Creative TV',
+  })
+)
 
 export default function App({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted()
@@ -15,15 +23,17 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Seo />
-      <Web3Provider>
-        {isMounted && (
-          <QueryClientProvider client={queryClient}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </QueryClientProvider>
-        )}
-      </Web3Provider>
+      {isMounted && (
+        <WagmiConfig client={wagmiClient}>
+          <ConnectKitProvider>
+            <QueryClientProvider client={queryClient}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </QueryClientProvider>
+          </ConnectKitProvider>
+        </WagmiConfig>
+      )}
     </ChakraProvider>
   )
 }
